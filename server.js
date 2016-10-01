b02d61b15cd4494ca53e896df1b35adc6346bfca
Aditya47 <adityakarodiya@gmail.com>
2016-10-01 15:07:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,13 +83,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var counter = 0;
-app.get('/counter', function(req,res){
-    counter = counter +1;
-    res.send(counter.toString());
-});
-
-
 var names = [];
 app.get('/submit-name',function(req,res){
     var name = req.query.name;
@@ -97,6 +90,13 @@ app.get('/submit-name',function(req,res){
     res.send(JSON.stringify(names));
     
 });
+
+var counter = 0;
+app.get('/counter', function(req,res){
+    counter = counter +1;
+    res.send(counter.toString());
+});
+
 
 
 app.get('/article-one', function (req, res) {
