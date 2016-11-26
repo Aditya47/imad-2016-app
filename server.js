464cb4c38e3b563fa70d36f4683a02eff1268254
Aditya47 <adityakarodiya@gmail.com>
2016-11-26 21:32:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,12 +87,12 @@ var htmlTemplate =
 
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'index6.html'));
 });
 
 
 app.get('/app1', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index6.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 var counter = 0;
